b3a80458b93b61f7d7ff2de06e5161239d0817a0
Gamaliel Quiroz <83394687+ghami20@users.noreply.github.com>
2023-09-22 09:26:51 -0300

    creacion de rama ghami

--- src/Clase10/Main.java
+++ src/Clase10/Main.java
@@ -7,6 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JOptionPane.showMessageDialog(null, "Hola Mundo");
+		
+		JOptionPane.showMessageDialog(null, "Estoy en la rama de ghami");
 	}
 
 }
