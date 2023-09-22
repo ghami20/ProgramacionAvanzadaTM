6db64b0da384f744d0b3f3a56808e658eb224cb1
Gamaliel Quiroz <83394687+ghami20@users.noreply.github.com>
2023-09-22 09:20:42 -0300

    inicio de repositorio
    Creacion de proyecto en java

---
+++ src/Clase10/Main.java
@@ -0,0 +1,12 @@
+package Clase10;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Hola Mundo");
+	}
+
+}
