396b6a77ff64d5151298a24a092eed18aa303ac0
Gamaliel Quiroz <83394687+ghami20@users.noreply.github.com>
2023-09-22 09:32:33 -0300

    Cambios eliminer for

--- src/Clase10/Main.java
+++ src/Clase10/Main.java
@@ -7,13 +7,6 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
-
-		
-		
-		for (Iterator iterator = collection.iterator(); iterator.hasNext();) {
-			type type = (type) iterator.next();
-			
-		}
 	}
 
 }
